9d3b94f7a4dba000bd223d2d6273a029e894bf5b
Alex Panchuk <apanchuk@gmail.com>
2017-11-25 21:10:13 +0500

    clean dev.js

--- dev.js
+++ dev.js
@@ -1,10 +1,2 @@
-// eslint-disable-next-line linebreak-style
 require("babel-register")
 require("./src/main")
-
-const foo = () =>
-  setTimeout(() => {
-    console.log("Hello")
-  }, 1000)
-
-export default foo
